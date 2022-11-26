b0a47cde6a24e64e40c74b7763a0e8c6a52ce449
Roland Kirchhoff <rolandkirchhoff@web.de>
2022-11-26 20:01:47 +0100

    Faves

---
+++ src/controllers/FavoritesController.js
@@ -0,0 +1,14 @@
+import pool from "../db/db.js";
+
+export const InsertFavorite = async (req, res, next) => { // not used by App but for testing purposes in the beginning
+    try {
+        console.log(req);
+        // const {rows: announcements} = await pool.query(`SELECT * FROM announcements WHERE city LIKE $1`,  ['%' + req.body.city + '%']);
+        //const {rows: announcements} = await pool.query(`SELECT * FROM announcements`);
+        // console.log(pool);
+        // res.status(200).json(announcements);
+    } catch (err) {
+        next(err);
+    }
+
+  };
